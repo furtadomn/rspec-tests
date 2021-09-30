a23ad192373b392af591bc1fc08993d067af1326
furtadomn <mnfurtado@yahoo.com.br>
2021-09-30 13:29:22 -0300

    adicionando testes

--- one-bit-code-rspec/test-like-a-hero/db/schema.rb
+++ one-bit-code-rspec/test-like-a-hero/db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_152716) do
+ActiveRecord::Schema.define(version: 2021_09_30_162040) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.integer "kind"
+    t.integer "level"
   end
 
 end
